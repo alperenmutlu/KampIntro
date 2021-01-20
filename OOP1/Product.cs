d68552fcc032b670289d83bfb58a44fb0bb7e9eb
alperenmutlu <chetto_17@hotmail.com>
2021-01-20 23:50:30 +0300

    20 Ocak Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+        
+        //ürünün birim fiyatı
+        public double UnitPrice { get; set; }
+        
+        //ürünün stok adedi
+        public int UnitsInStock { get; set; }
+
+        //CRUD,create,read,update,delete
+
+    }
+}
