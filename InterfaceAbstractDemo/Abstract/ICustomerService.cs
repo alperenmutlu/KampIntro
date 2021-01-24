eef3aebd064afa69dc73f4591a4e9801db6aa02d
alperenmutlu <chetto_17@hotmail.com>
2021-01-25 01:35:07 +0300

    --

---
+++ InterfaceAbstractDemo/Abstract/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Abstract
+{
+    public interface ICustomerService
+    {
+        void Save(Customer customer);
+    }
+}
