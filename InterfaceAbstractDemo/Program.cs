face65af4c0f7f3bb196715ae1267994cf05a1bd
alperenmutlu <chetto_17@hotmail.com>
2021-01-25 01:35:07 +0300

    --

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1998, 1, 1), FirstName = "The Beast", LastName = "The Best", NationalityId = "11111111111" });
+            Console.ReadLine();
+        }
+    }
+}
