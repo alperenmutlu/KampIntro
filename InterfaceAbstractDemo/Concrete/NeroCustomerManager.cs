c9cb99783ea0d0aab1147d5eab263ce18ef53be0
alperenmutlu <chetto_17@hotmail.com>
2021-01-25 01:35:07 +0300

    InterfaceAbstract-KimlikDoğrulama

---
+++ InterfaceAbstractDemo/Concrete/NeroCustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class NeroCustomerManager:BaseCustomerManager
+    {
+        ICustomerCheckService _customerCheckService;
+
+        public NeroCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+        public override void Save(Customer customer)
+        {
+
+            if (_customerCheckService.CheckIfRealPerson(customer))
+            {
+                base.Save(customer);
+            }
+            else
+            {
+                throw new Exception("Not a valid Person");
+            }
+        }
+    }
+}
